045793722f6b407e4d5b2a8cf016f308ef183e3a
Danyyl <danyyl.con@gmail.com>
2022-02-14 13:28:08 +0200

    create project/add first component, model and service

---
+++ src/app/models/Product.ts
@@ -0,0 +1,15 @@
+export class Product{
+    id: number;
+    name: string;
+    price: number;
+    url: string;
+    description: string;
+
+    constructor(){
+        this.id = 1;
+        this.name = 'Product1';
+        this.price = 10;
+        this.url = 'https://www.google.com.ua'
+        this.description = 'Just a product'
+    }
+}
