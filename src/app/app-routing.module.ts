95910b240ff0629f39e9b29801e246df6de0bfaf
Danyyl <danyyl.con@gmail.com>
2022-02-15 13:51:55 +0200

    create productItem component and use service to find right product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductListComponent},
+  {path: 'products/:id', component: ProductItemComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/product-item/product-item.component.ts
@@ -0,0 +1,30 @@
+import { typeofExpr } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/Product';
+import { ProductsService } from '../services/products.service';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+  id: Number = 0;
+  product: Product;
+
+  constructor(private activatedRoute: ActivatedRoute, private productsService: ProductsService) {   
+  }
+
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.productsService.getProducts().subscribe(products => {
+      this.product = products.find(p => p.id === this.id) as Product
+      console.log(this.product.url)
+    })
+    
+
+  }
+
+}
